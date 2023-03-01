32c6719901cb84c7bc7e2b08968b212ddee99588
Adrian <adrian.niwa@inox.vision>
2023-03-01 19:57:19 +0100

    organizing synchronisation parameters

--- 02 Mirror_Synchroniser/synchroniser.py
+++ 02 Mirror_Synchroniser/synchroniser.py
@@ -1,14 +1,14 @@
 from synchroniser_module import MirrorSynchroniser
 
 
-source_dir = '/home/adrian/Insync/blindesign.pl@gmail.com/OneDrive/PROGRAMOWANIE/Python/UNDER DEVELOPMENT/Synchroniser/synchro_A'
-dest_dir = '/home/adrian/Insync/blindesign.pl@gmail.com/OneDrive/PROGRAMOWANIE/Python/UNDER DEVELOPMENT/Synchroniser/synchro_B'
-ignore_list = []
-use_history = True
-days_to_backup = 14
-
+synchro_list = {
+    'source_dir' : '<source_dir_here>',
+    'dest_dir' : '<destination_dir_here>',
+    'ignore_list' : [],
+    'use_history' : True,
+    'days_to_backup' : 14,
+}
    
-synchroniser = MirrorSynchroniser(source_dir, dest_dir, ignore_list, use_history, days_to_backup)
-
+synchroniser = MirrorSynchroniser(**synchro_list)
 
 synchroniser.synchronise()
